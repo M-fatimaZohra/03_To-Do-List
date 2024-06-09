4be0a4754addc92ac27c1055584315487b76a152
M.A COM <fatima.zohra101220007@gmail.com>
2024-06-08 22:08:26 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+
+let toDos:string[]=[]
+let condition = true
+
+while(condition){
+  
+let answer = await inquirer.prompt([
+    {
+      name:"todo",
+      type:"input",
+      message:"•What Would you like to add in todos?"
+    }
+])
+
+toDos.push(answer.todo);
+
+let nextStep = await inquirer.prompt(
+  [
+    {
+      name:"more",
+      type:"confirm",
+      message:"•would you like to add more toDos?",
+      default:"true"
+  }
+  ]
+)
+condition = nextStep.more
+
+if (condition){
+  console.log(toDos)
+}
+
+else {
+  console.log(`📜TODAY TODO LIST:`);
+  
+  for(let work of toDos){
+    console.log(`•) ${work}`)
+  }
+}
+
+}
